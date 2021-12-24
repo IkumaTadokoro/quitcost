ebb4277a8f9ab0680ccfbe6ed0180c16225071e5
ikuma-t <tadokorodev@gmail.com>
2021-12-24 12:01:20 +0900

    purgeオプションをcontentオプションに変更し、内容を整理
    - TailwindCSS3.0ではPurgeCSSを使用しなくなり、オプション名の変更が加えられたため
    - 使用しないerbを削除して、slimとvueを有効化した

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  purge: [
-    './app/views/**/*.html.erb',
+  content: [
+    './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
+    './app/javascript/**/*.vue'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
