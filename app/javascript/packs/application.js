3c1423f41e26e432c8981c5dd9a769f1b3cfdbf0
ikuma-t <tadokorodev@gmail.com>
2021-12-24 11:12:46 +0900

    Lintの指摘を修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "stylesheets/application.css"
+import Rails from '@rails/ujs'
+import Turbolinks from 'turbolinks'
+import * as ActiveStorage from '@rails/activestorage'
+import 'channels'
+import 'stylesheets/application.css'
 
 Rails.start()
 Turbolinks.start()
