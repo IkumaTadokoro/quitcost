14f71d4b5400ba96fdb7f452912cad9745e75891
ikuma-t <tadokorodev@gmail.com>
2022-01-31 13:51:37 +0900

    feat: 国民健康保険料登録機能のテストを作成
    InsuranceForm#saveの例外処理に関するテスト方針がわからなかったため、一旦保留としている

---
+++ spec/models/insurance_form_spec.rb
@@ -0,0 +1,245 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe InsuranceForm, type: :model do
+  describe '#validate' do
+    describe ':year' do
+      subject { insurance_form.errors[:year] }
+      before { insurance_form.valid? }
+      let(:insurance_form) { build(:insurance_form, year: year) }
+
+      context 'when year is nil' do
+        let(:year) { nil }
+        it { is_expected.to include 'を入力してください' }
+      end
+
+      context 'when year is negative' do
+        let(:year) { -1 }
+        it { is_expected.to include 'は0以上の値にしてください' }
+      end
+
+      context 'when year is 0' do
+        let(:year) { 0 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when year is 1' do
+        let(:year) { 1 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when year is decimal' do
+        let(:year) { 1.0 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when year is string' do
+        let(:year) { '２０００' }
+        it { is_expected.to be_empty }
+      end
+    end
+
+    describe ':local_gov_code' do
+      subject { insurance_form.errors[:local_gov_code] }
+      let(:insurance_form) { build(:insurance_form, params) }
+
+      context 'when local_gov_code is nil' do
+        let(:params) { { local_gov_code: nil } }
+        before { insurance_form.valid? }
+        it { is_expected.to include 'を入力してください' }
+      end
+
+      context 'when local_gov_code DOES NOT meet JIS Standard' do
+        let(:params) { { local_gov_code: '131017' } }
+        before { insurance_form.valid? }
+        it { is_expected.to include 'に無効な値が設定されています' }
+      end
+
+      context 'when duplicate record is existing' do
+        let(:params) { { year: 2000, local_gov_code: '131016' } }
+        before do
+          create(:insurance, year: 2000, local_gov_code: '131016')
+          insurance_form.valid?
+        end
+        it { is_expected.to include 'に既に登録済みのコードが指定されています。年度を見直してください' }
+      end
+    end
+
+    describe ':income_basis' do
+      subject { insurance_form.errors[:medical_income_basis] }
+      before { insurance_form.valid? }
+      let(:insurance_form) { build(:insurance_form, medical_income_basis: medical_income_basis) }
+
+      context 'when medical_income_basis is nil' do
+        let(:medical_income_basis) { nil }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_income_basis is less than 0' do
+        let(:medical_income_basis) { -1 }
+        it { is_expected.to include 'は0以上の値にしてください' }
+      end
+
+      context 'when medical_income_basis is 0' do
+        let(:medical_income_basis) { 0 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_income_basis is 0 ~ 100' do
+        let(:medical_income_basis) { 50.05 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_income_basis is 99' do
+        let(:medical_income_basis) { 99 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_income_basis is 100' do
+        let(:medical_income_basis) { 100 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_income_basis is greater than 100' do
+        let(:medical_income_basis) { 101 }
+        it { is_expected.to include 'は100以下の値にしてください' }
+      end
+    end
+
+    describe ':asset_basis' do
+      subject { insurance_form.errors[:medical_asset_basis] }
+      before { insurance_form.valid? }
+      let(:insurance_form) { build(:insurance_form, medical_asset_basis: medical_asset_basis) }
+
+      context 'when medical_asset_basis is nil' do
+        let(:medical_asset_basis) { nil }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_asset_basis is less than 0' do
+        let(:medical_asset_basis) { -1 }
+        it { is_expected.to include 'は0以上の値にしてください' }
+      end
+
+      context 'when medical_asset_basis is 0' do
+        let(:medical_asset_basis) { 0 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_asset_basis is 0 ~ 100' do
+        let(:medical_asset_basis) { 50.05 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_asset_basis is 99' do
+        let(:medical_asset_basis) { 99 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_asset_basis is 100' do
+        let(:medical_asset_basis) { 100 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_asset_basis is greater than 100' do
+        let(:medical_asset_basis) { 101 }
+        it { is_expected.to include 'は100以下の値にしてください' }
+      end
+    end
+
+    describe ':capita_basis' do
+      subject { insurance_form.errors[:medical_capita_basis] }
+      before { insurance_form.valid? }
+      let(:insurance_form) { build(:insurance_form, medical_capita_basis: medical_capita_basis) }
+
+      context 'when medical_capita_basis is nil' do
+        let(:medical_capita_basis) { nil }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_capita_basis is less than 0' do
+        let(:medical_capita_basis) { -1 }
+        it { is_expected.to include 'は0以上の値にしてください' }
+      end
+
+      context 'when medical_capita_basis is 0' do
+        let(:medical_capita_basis) { 0 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_capita_basis is over 0' do
+        let(:medical_capita_basis) { 1 }
+        it { is_expected.to be_empty }
+      end
+    end
+
+    describe ':household_basis' do
+      subject { insurance_form.errors[:medical_household_basis] }
+      before { insurance_form.valid? }
+      let(:insurance_form) { build(:insurance_form, medical_household_basis: medical_household_basis) }
+
+      context 'when medical_household_basis is nil' do
+        let(:medical_household_basis) { nil }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_household_basis is less than 0' do
+        let(:medical_household_basis) { -1 }
+        it { is_expected.to include 'は0以上の値にしてください' }
+      end
+
+      context 'when medical_household_basis is 0' do
+        let(:medical_household_basis) { 0 }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when medical_household_basis is over 0' do
+        let(:medical_household_basis) { 1 }
+        it { is_expected.to be_empty }
+      end
+    end
+  end
+
+  describe '#save' do
+    subject { insurance_form.save }
+
+    context 'when params are valid' do
+      context 'when success to save' do
+        let!(:insurance_form) { build(:insurance_form, :all_months_are_target) }
+        it { expect { subject }.to change { Insurance.count }.from(0).to(1) }
+        it { expect { subject }.to change { PaymentTargetMonth.count }.from(0).to(12) }
+      end
+
+      context 'when failed to save' do
+        context 'when record is invalid' do
+          # TODO: 例外処理のテスト方法がわからないのであとで実装する
+          xit 'raises ActiveRecord::RecordInvalid' do
+            insurance_form_mock = double('Insurance Form')
+            allow(insurance_form_mock).to receive(:save).and_raise(ActiveRecord::RecordInvalid)
+            insurance_form = build(:insurance_form)
+            allow(insurance_form).to receive(:save).and_return(insurance_form_mock.save)
+            expect(insurance_form.save).to be_falsey
+          end
+        end
+      end
+    end
+
+    context 'when params are INVALID' do
+      let(:insurance_form) { build(:insurance_form, year: nil) }
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe '#to_model' do
+    subject { insurance_form.to_model == insurance_form.insurance }
+    let(:insurance_form) { build(:insurance_form) }
+    it { is_expected.to be_truthy }
+  end
+
+  describe '.calendars' do
+    subject { InsuranceForm.calendars }
+    let!(:expected) { %w[month1 month2 month3 month4 month5 month6 month7 month8 month9 month10 month11 month12] }
+    it { is_expected.to eq expected }
+  end
+end
--- spec/system/insurance_spec.rb
+++ spec/system/insurance_spec.rb
@@ -10,4 +10,50 @@ RSpec.describe 'Insurance', type: :system, js: true do
       expect(page).to have_content '国民健康保険料一覧'
     end
   end
+
+  describe 'create' do
+    let(:insurance_form) { build(:insurance_form, :all_months_are_target) }
+    scenario 'create a new record' do
+      visit insurances_path
+      click_link '新規登録'
+
+      expect(page).to have_content '保険料新規登録'
+
+      fill_in '年度', with: insurance_form.year
+      select JpLocalGov.find(insurance_form.local_gov_code).city, from: '市区町村名'
+      fill_in '所得割（医療分）', with: insurance_form.medical_income_basis
+      fill_in '資産割（医療分）', with: insurance_form.medical_asset_basis
+      fill_in '均等割（医療分）', with: insurance_form.medical_capita_basis
+      fill_in '平等割（医療分）', with: insurance_form.medical_household_basis
+      fill_in '限度額（医療分）', with: insurance_form.medical_limit
+      fill_in '所得割（後期高齢者支援分）', with: insurance_form.elderly_income_basis
+      fill_in '資産割（後期高齢者支援分）', with: insurance_form.elderly_asset_basis
+      fill_in '均等割（後期高齢者支援分）', with: insurance_form.elderly_capita_basis
+      fill_in '平等割（後期高齢者支援分）', with: insurance_form.elderly_household_basis
+      fill_in '限度額（後期高齢者支援分）', with: insurance_form.elderly_limit
+      fill_in '所得割（介護分）', with: insurance_form.care_income_basis
+      fill_in '資産割（介護分）', with: insurance_form.care_asset_basis
+      fill_in '均等割（介護分）', with: insurance_form.care_capita_basis
+      fill_in '平等割（介護分）', with: insurance_form.care_household_basis
+      fill_in '限度額（介護分）', with: insurance_form.care_limit
+      check '1月', allow_label_click: true
+      check '2月', allow_label_click: true
+      check '3月', allow_label_click: true
+      check '4月', allow_label_click: true
+      check '5月', allow_label_click: true
+      check '6月', allow_label_click: true
+      check '7月', allow_label_click: true
+      check '8月', allow_label_click: true
+      check '9月', allow_label_click: true
+      check '10月', allow_label_click: true
+      check '11月', allow_label_click: true
+      check '12月', allow_label_click: true
+
+      expect { click_button '新規作成' }
+        .to change { Insurance.count }.from(0).to(1)
+                                      .and change { PaymentTargetMonth.count }.from(0).to(12)
+      assert_current_path insurances_path
+      assert_text '保険料率を保存しました。'
+    end
+  end
 end
