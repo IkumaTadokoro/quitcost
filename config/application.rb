99e6a9dd51729c34960edc892d94107600e8253e
ikuma-t <tadokorodev@gmail.com>
2022-02-09 13:01:30 +0900

    chore: TimeZoneをTokyoに設定

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Quitcost
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
