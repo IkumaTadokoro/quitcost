f7f5d3719e25d45f26c5647780159f938210c6ab
ikuma-t <tadokorodev@gmail.com>
2022-02-23 13:37:52 +0900

    feat: Vue Routerのルーティングをアプリ全体で使えるように修正
    FormWizardコンポーネント内だけでルーティングを行っていたが、
    計算結果やホーム画面もルーティングで切り替えるため、従来のルートをネストしたルート
    に変更し、ページそのもの自体を切り替えられ流ように変更した

--- app/javascript/src/simulation.js
+++ app/javascript/src/simulation.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import SimulationForm from './SimulationForm.vue'
+import App from './App.vue'
 import Maska from 'maska'
 import router from './router.js'
 
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const selector = '#js-simulation'
   const simulation = document.querySelector(selector)
   if (simulation) {
-    const app = createApp(SimulationForm)
+    const app = createApp(App)
     app.use(Maska)
     app.use(router)
     app.mount(selector)
