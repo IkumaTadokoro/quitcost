646fe590ae06387ecd9896b66c5b6e186d733809
ikuma-t <tadokorodev@gmail.com>
2022-02-23 09:15:45 +0900

    feat: ルーティング設定を追加
    simulations/new以下については、Vue Router側でURL制御するため、
    Rails側のルーティンググロブでリダイレクトを設定している
    今後SPAの範囲が拡大する場合は、適宜修正すること

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :simulations, only: %i(new)
   resources :insurances, only: %i(index new create edit update)
   resources :pensions, only: %i(index new create edit update)
+  get 'simulations/new/*path', to: 'simulations#new'
   namespace :api do
     resource :simulations, only: %i(show)
     resources :insurances, only: %i(index destroy)
