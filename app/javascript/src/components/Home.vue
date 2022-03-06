5376fa3eae7cdbbefa738152a5ccc96a53fb4bbc
ikuma-t <tadokorodev@gmail.com>
2022-03-06 19:04:36 +0900

    feat(home): home画面を作成（レスポンシブ対応済）
    ## 申し送り事項
    
    「準備するもの」画像はダミーであるため、あとで追加してください

---
+++ app/javascript/src/components/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="container mx-auto pt-8 pb-6">
+      <div class="md:max-w-3xl mb-6 mx-auto px-4">
+        <img src="~logo.svg" alt="" />
+      </div>
+      <div class="md:max-w-6xl mb-6 mx-auto bg-secondary p-2">
+        <p class="text-center text-xs md:text-base">
+          転職準備のために仕事をやめたいけど、お金が心配...。
+        </p>
+        <p class="text-center text-xs md:text-base">
+          8つの質問に答えるだけで、無職期間でどれだけ「保険」「税金」「年金」がかかるか計算できます
+        </p>
+      </div>
+      <div class="flex justify-center">
+        <button
+          class="border-0 w-40 md:w-56 p-4 md:p-6 focus:outline-none rounded-full text-sm md:text-lg bg-primary text-white hover:bg-accent shadow-xl"
+          @click="moveForm"
+        >
+          いますぐ計算する
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="bg-boundaryBlack">
+    <div class="container mx-auto py-8">
+      <div
+        class="md:max-w-6xl mx-auto bg-white md:rounded-lg px-4 md:px-12 py-6"
+      >
+        <div class="mb-6">
+          <h2
+            class="text-lg md:text-2xl text-center underline underline-offset-8 decoration-4 decoration-primary"
+          >
+            準備するもの
+          </h2>
+        </div>
+        <div class="flex flex-wrap gap-y-4">
+          <div class="w-full md:w-1/2 px-4">
+            <p class="text-sm md:text-lg text-center mb-2 text-gray">
+              住民税決定通知書
+            </p>
+            <img src="~dummy.svg" alt="" class="block mx-auto" />
+          </div>
+          <div class="w-full md:w-1/2 px-4">
+            <p class="text-sm md:text-lg text-center mb-2 text-gray">
+              今年1月から現在までの給与明細
+            </p>
+            <img src="~dummy.svg" alt="" class="block mx-auto" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import 'logo.svg'
+import 'dummy.svg'
+import { useRouter } from 'vue-router'
+import { useGlobalStore } from '../store/global'
+
+const router = useRouter()
+const { simulation, step } = useGlobalStore()
+
+const moveForm = () => {
+  router.push('/simulations/new')
+  step.reset()
+  simulation.reset_params()
+  simulation.reset_result()
+}
+</script>
