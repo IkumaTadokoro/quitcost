68a50a9b42a1dea7a31a9c6150c314b9bd2c9cb3
ikuma-t <tadokorodev@gmail.com>
2022-03-12 15:02:28 +0900

    feat(useFinancialYear): 翌年度の会計年度の始端終端を取得できるようにした
    合わせて月計算の値が間違っているのを修正

--- app/javascript/src/composables/use-financial-year.js
+++ app/javascript/src/composables/use-financial-year.js
@@ -1,4 +1,10 @@
-import { addMonths, subMonths, subYears, lastDayOfMonth } from 'date-fns'
+import {
+  addMonths,
+  subMonths,
+  addYears,
+  subYears,
+  lastDayOfMonth
+} from 'date-fns'
 
 export const useFinancialYear = (
   date,
@@ -8,19 +14,23 @@ export const useFinancialYear = (
   const year =
     date_beginning_of_fiscal_year === 1
       ? date.getFullYear()
-      : subMonths(date, date_beginning_of_fiscal_year).getFullYear()
+      : subMonths(date, date_beginning_of_fiscal_year - 1).getFullYear()
   const beginningOfYear = new Date(year, beginning_of_fiscal_year - 1, 1)
+  const nextBeginningOfYear = addYears(beginningOfYear, 1)
   const lastBeginningOfYear = subYears(beginningOfYear, 1)
   const beforeLastBeginningOfYear = subYears(beginningOfYear, 2)
   const endOfYear = lastDayOfMonth(addMonths(beginningOfYear, 11))
+  const nextEndOfYear = addYears(endOfYear, 1)
   const lastEndOfYear = subYears(endOfYear, 1)
   const beforeLastEndOfYear = subYears(endOfYear, 2)
 
   return {
     beginningOfYear,
+    nextBeginningOfYear,
     lastBeginningOfYear,
     beforeLastBeginningOfYear,
     endOfYear,
+    nextEndOfYear,
     lastEndOfYear,
     beforeLastEndOfYear,
     year
