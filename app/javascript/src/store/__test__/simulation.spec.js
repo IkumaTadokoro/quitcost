eabce09bac3da9729c3f7375821fb8484f844235
ikuma-t <tadokorodev@gmail.com>
2022-03-14 20:04:00 +0900

    test(store): store/simulationのテストを追加
    private領域にかなりの行数のコードがあるため、一旦テスト可能な範囲まで追加
    コンポーネントのテストと合わせて随時修正する

---
+++ app/javascript/src/store/__test__/simulation.spec.js
@@ -0,0 +1,34 @@
+import simulationStore from '../simulation'
+
+describe('#add_params', () => {
+  const mockDate = new Date('2022-03-15')
+  jest.spyOn(global, 'Date').mockImplementation(() => mockDate)
+  const simulation = simulationStore()
+
+  it('add value at the end of params', () => {
+    expect(simulation.params).toEqual({
+      simulationDate: mockDate
+    })
+
+    const values = { employmentMonth: '2022/03' }
+    simulation.add_params(values)
+    expect(simulation.params).toEqual({
+      simulationDate: mockDate,
+      employmentMonth: '2022/03'
+    })
+  })
+})
+
+describe('#reset', () => {
+  const mockDate = new Date('2022-03-15')
+  jest.spyOn(global, 'Date').mockImplementation(() => mockDate)
+  const simulation = simulationStore()
+
+  it('reset params, result, routes and currentStep', () => {
+    simulation.reset()
+    expect(simulation.params).toEqual({ simulationDate: mockDate })
+    expect(simulation.result).toEqual({})
+    expect(simulation.steps).toEqual(10)
+    expect(simulation.currentStepIdx).toEqual(0)
+  })
+})
