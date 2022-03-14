048f497bda0bf87ac0957f119dd7ccd99f453f41
ikuma-t <tadokorodev@gmail.com>
2022-03-14 14:53:02 +0900

    fix(validation): 必須チェックが型エラーに引っかからないように変更

--- app/javascript/src/composables/useValidationSchema.js
+++ app/javascript/src/composables/useValidationSchema.js
@@ -12,8 +12,11 @@ export const useValidationSchema = (baseDate) => {
     RetirementMonth: yup.object({
       retirementMonth: yup
         .date()
+        .nullable()
+        .transform((value, originalValue) =>
+          originalValue === '' ? null : value
+        )
         .required('退職予定月は必須です')
-        .typeError('無効な日付です。YYYY/MMの形式で入力してください。')
         .min(
           computableFrom,
           `退職予定月には ${computableFrom} 以降の月を指定してください`
@@ -22,13 +25,17 @@ export const useValidationSchema = (baseDate) => {
           computableTo,
           `退職予定月には ${computableTo} 以前の月を指定してください`
         )
+        .typeError('無効な日付です。YYYY/MMの形式で入力してください。')
     }),
     EmploymentMonth: yup.object({
       retirementMonth: yup.date(),
       employmentMonth: yup
         .date()
+        .nullable()
+        .transform((value, originalValue) =>
+          originalValue === '' ? null : value
+        )
         .required('転職予定月は必須です')
-        .typeError('無効な日付です。YYYY/MMの形式で入力してください。')
         .min(
           yup.ref('retirementMonth'),
           `転職予定月には、退職予定月以降の月を指定してください`
@@ -37,14 +44,16 @@ export const useValidationSchema = (baseDate) => {
           computableTo,
           `転職予定月には ${computableTo} 以前の月を指定してください`
         )
+        .typeError('無効な日付です。YYYY/MMの形式で入力してください。')
     }),
     Age: yup.object({
       age: yup
         .number()
+        .transform((value) => (isNaN(value) ? undefined : value))
         .required('年齢は必須です')
-        .typeError('無効な数値です。')
         .min(0, '0以上の整数を入力してください')
         .integer('整数で入力してください')
+        .typeError('無効な数値です。')
     }),
     PostalCode: yup.object({
       postalCode: yup
@@ -59,50 +68,56 @@ export const useValidationSchema = (baseDate) => {
     PreviousSalary: yup.object({
       previousSalary: yup
         .number()
+        .transform((value) => (isNaN(value) ? undefined : value))
         .required('昨昨年度の所得は必須です')
-        .typeError('無効な数値です。')
         .min(0, '0以上の整数を入力してください')
         .integer('整数で入力してください')
+        .typeError('無効な数値です。')
     }),
     PreviousSocialInsurance: yup.object({
       previousSocialInsurance: yup
         .number()
+        .transform((value) => (isNaN(value) ? undefined : value))
         .required('昨昨年度の社会保険料は必須です')
-        .typeError('無効な数値です。')
         .min(0, '0以上の整数を入力してください')
         .integer('整数で入力してください')
+        .typeError('無効な数値です。')
     }),
     Salary: yup.object({
       salary: yup
         .number()
+        .transform((value) => (isNaN(value) ? undefined : value))
         .required('昨年度の所得は必須です')
-        .typeError('無効な数値です。')
         .min(0, '0以上の整数を入力してください')
         .integer('整数で入力してください')
+        .typeError('無効な数値です。')
     }),
     SocialInsurance: yup.object({
       socialInsurance: yup
         .number()
+        .transform((value) => (isNaN(value) ? undefined : value))
         .required('昨年度の社会保険料は必須です')
-        .typeError('無効な数値です。')
         .min(0, '0以上の整数を入力してください')
         .integer('整数で入力してください')
+        .typeError('無効な数値です。')
     }),
     ScheduledSalary: yup.object({
       scheduledSalary: yup
         .number()
+        .transform((value) => (isNaN(value) ? undefined : value))
         .required('今年度の所得は必須です')
-        .typeError('無効な数値です。')
         .min(0, '0以上の整数を入力してください')
         .integer('整数で入力してください')
+        .typeError('無効な数値です。')
     }),
     ScheduledSocialInsurance: yup.object({
       scheduledSocialInsurance: yup
         .number()
+        .transform((value) => (isNaN(value) ? undefined : value))
         .required('今年度の社会保険料は必須です')
-        .typeError('無効な数値です。')
         .min(0, '0以上の整数を入力してください')
         .integer('整数で入力してください')
+        .typeError('無効な数値です。')
     })
   }
 
