4ef596c62ffcafa7667af61b65d4dd5ab60adb47
ikuma-t <tadokorodev@gmail.com>
2022-03-14 15:34:56 +0900

    refactor(validation): validationSchemaはチェック対象のみを保持するように修正
    validationSchemaにチェック対象とチェック内容の双方を含んでいると、全体としてどのカラムにチェックがかかっているかがわかりづらい
    そのためvalidationSchemaが保持しているチェック内容を、各メソッドに抽出した

--- app/javascript/src/composables/useValidationSchema.js
+++ app/javascript/src/composables/useValidationSchema.js
@@ -26,75 +26,95 @@ export const useValidationSchema = (baseDate) => {
       .required(`${columnName}は必須です`)
   }
 
+  const RetirementMonth = yup.object({
+    retirementMonth: datePresence('退職予定月')
+      .min(
+        computableFrom,
+        `退職予定月には ${computableFrom} 以降の月を指定してください`
+      )
+      .max(
+        computableTo,
+        `退職予定月には ${computableTo} 以前の月を指定してください`
+      )
+  })
+
+  const EmploymentMonth = yup.object({
+    retirementMonth: yup.date(),
+    employmentMonth: datePresence('転職予定月')
+      .min(
+        yup.ref('retirementMonth'),
+        `転職予定月には、退職予定月以降の月を指定してください`
+      )
+      .max(
+        computableTo,
+        `転職予定月には ${computableTo} 以前の月を指定してください`
+      )
+  })
+
+  const Age = yup.object({
+    age: numberPresence('年齢')
+      .min(0, '0以上の整数を入力してください')
+      .integer('整数で入力してください')
+  })
+
+  const PostalCode = yup.object({
+    postalCode: yup
+      .string()
+      .matches(/^[0-9]{3}-[0-9]{4}$/, {
+        message: '7桁の郵便番号を入力してください',
+        excludeEmptyString: true
+      })
+      .required('郵便番号は必須です'),
+    address: yup.string().required('該当する市区町村がありません')
+  })
+
+  const PreviousSalary = yup.object({
+    previousSalary: numberPresence('昨昨年度の所得')
+      .min(0, '0以上の整数を入力してください')
+      .integer('整数で入力してください')
+  })
+
+  const PreviousSocialInsurance = yup.object({
+    previousSocialInsurance: numberPresence('昨昨年度の社会保険料')
+      .min(0, '0以上の整数を入力してください')
+      .integer('整数で入力してください')
+  })
+
+  const Salary = yup.object({
+    salary: numberPresence('昨年度の所得')
+      .min(0, '0以上の整数を入力してください')
+      .integer('整数で入力してください')
+  })
+
+  const SocialInsurance = yup.object({
+    socialInsurance: numberPresence('昨年度の社会保険料')
+      .min(0, '0以上の整数を入力してください')
+      .integer('整数で入力してください')
+  })
+
+  const ScheduledSalary = yup.object({
+    scheduledSalary: numberPresence('今年度の所得')
+      .min(0, '0以上の整数を入力してください')
+      .integer('整数で入力してください')
+  })
+
+  const ScheduledSocialInsurance = yup.object({
+    scheduledSocialInsurance: numberPresence('今年度の社会保険料')
+      .min(0, '0以上の整数を入力してください')
+      .integer('整数で入力してください')
+  })
+
   const validationSchema = {
-    RetirementMonth: yup.object({
-      retirementMonth: datePresence('退職予定月')
-        .min(
-          computableFrom,
-          `退職予定月には ${computableFrom} 以降の月を指定してください`
-        )
-        .max(
-          computableTo,
-          `退職予定月には ${computableTo} 以前の月を指定してください`
-        )
-    }),
-    EmploymentMonth: yup.object({
-      retirementMonth: yup.date(),
-      employmentMonth: datePresence('転職予定月')
-        .min(
-          yup.ref('retirementMonth'),
-          `転職予定月には、退職予定月以降の月を指定してください`
-        )
-        .max(
-          computableTo,
-          `転職予定月には ${computableTo} 以前の月を指定してください`
-        )
-    }),
-    Age: yup.object({
-      age: numberPresence('年齢')
-        .min(0, '0以上の整数を入力してください')
-        .integer('整数で入力してください')
-    }),
-    PostalCode: yup.object({
-      postalCode: yup
-        .string()
-        .matches(/^[0-9]{3}-[0-9]{4}$/, {
-          message: '7桁の郵便番号を入力してください',
-          excludeEmptyString: true
-        })
-        .required('郵便番号は必須です'),
-      address: yup.string().required('該当する市区町村がありません')
-    }),
-    PreviousSalary: yup.object({
-      previousSalary: numberPresence('昨昨年度の所得')
-        .min(0, '0以上の整数を入力してください')
-        .integer('整数で入力してください')
-    }),
-    PreviousSocialInsurance: yup.object({
-      previousSocialInsurance: numberPresence('昨昨年度の社会保険料')
-        .min(0, '0以上の整数を入力してください')
-        .integer('整数で入力してください')
-    }),
-    Salary: yup.object({
-      salary: numberPresence('昨年度の所得')
-        .min(0, '0以上の整数を入力してください')
-        .integer('整数で入力してください')
-    }),
-    SocialInsurance: yup.object({
-      socialInsurance: numberPresence('昨年度の社会保険料')
-        .min(0, '0以上の整数を入力してください')
-        .integer('整数で入力してください')
-    }),
-    ScheduledSalary: yup.object({
-      scheduledSalary: numberPresence('今年度の所得')
-        .min(0, '0以上の整数を入力してください')
-        .integer('整数で入力してください')
-    }),
-    ScheduledSocialInsurance: yup.object({
-      scheduledSocialInsurance: numberPresence('今年度の社会保険料')
-        .min(0, '0以上の整数を入力してください')
-        .integer('整数で入力してください')
-    })
+    RetirementMonth,
+    EmploymentMonth,
+    Age,
+    PostalCode,
+    PreviousSalary,
+    PreviousSocialInsurance,
+    Salary,
+    SocialInsurance,
+    ScheduledSalary,
+    ScheduledSocialInsurance
   }
 
   return validationSchema
