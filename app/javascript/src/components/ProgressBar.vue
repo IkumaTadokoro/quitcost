63d809e62fbf87736d742ce7c6bcf21abbe58333
ikuma-t <tadokorodev@gmail.com>
2022-03-16 14:11:39 +0900

    test(ProgressBar): ProgressBarのテストを追加
    テストに際し、テスト用のdata属性を追加

--- app/javascript/src/components/ProgressBar.vue
+++ app/javascript/src/components/ProgressBar.vue
@@ -5,15 +5,13 @@
         v-if="progress"
         class="bg-primary text-xs font-medium text-primary text-center p-0.5 leading-none rounded-full"
         :style="{ width: progress + '%' }"
-      >
-        1
-      </div>
+        data-test-id="Progress"
+      ></div>
       <div
         v-else
-        class="bg-secondary text-xs font-medium text-secondary text-left py-0.5 pl-8 leading-none rounded-full"
-      >
-        0
-      </div>
+        class="bg-secondary text-xs font-medium text-secondary text-left py-0.5 pl-8 leading-none rounded-full h-3"
+        data-test-id="Steps"
+      ></div>
     </div>
   </div>
 </template>
