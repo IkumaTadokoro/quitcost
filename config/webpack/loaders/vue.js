d29a7bee7c6f2a0a619bd34997190493772db515
ikuma-t <tadokorodev@gmail.com>
2022-03-16 13:11:23 +0900

    build(vue): production環境でのみdata-test-id属性を除外する設定
    tailwindCSSを利用している関係で、各要素がCSSで識別しづらくテストが実行しづらい
    そのためテスト用のデータ属性`data-test-id`を付与したいが、productionでは不要であるため、コンパイル時に除去する設定を追加

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,7 +1,31 @@
+function removeAttr(astEl, name) {
+  const { attrsList, attrsMap } = astEl
+  if (attrsMap[name]) {
+    delete attrsMap[name]
+    const index = attrsList.findIndex((x) => x.name === name)
+    attrsList.splice(index, 1)
+  }
+  return astEl
+}
+
+function removeTestAttr(astEl) {
+  removeAttr(astEl, 'data-test-id')
+  removeAttr(astEl, ':data-test-id')
+  return astEl
+}
+
+const compileModules =
+  process.env.NODE_ENV === 'production'
+    ? [{ preTransformNode: removeTestAttr }]
+    : []
+
 module.exports = {
   test: /\.vue$/,
   loader: 'vue-loader',
   options: {
-    reactivityTransform: true
+    reactivityTransform: true,
+    compilerOptions: {
+      modules: compileModules
+    }
   }
 }
