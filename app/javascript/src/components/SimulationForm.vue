68d83a0b5ec69a9cf8bfa47a277618f33a27a7fe
ikuma-t <tadokorodev@gmail.com>
2022-03-21 08:42:56 +0900

    chore(SimulationForm): 重複しているレンダリングを削除

--- app/javascript/src/components/SimulationForm.vue
+++ app/javascript/src/components/SimulationForm.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="container mx-auto">
-    <FormWizard @submit="onSubmit">
-      <router-view></router-view>
-    </FormWizard>
+    <FormWizard @submit="onSubmit" />
   </div>
 </template>
 
@@ -17,19 +15,27 @@ const onSubmit = () => router.push('/simulations')
 
 <style scope>
 .form-label {
-  @apply text-4xl inline-block mb-12;
+  @apply h-20 text-4xl inline-block mb-2;
 }
 
 .form-field {
-  @apply px-6 py-2 w-9/12 rounded-md border-4 border-boundaryBlack focus:border-primary tracking-widest text-5xl outline-none leading-8 transition-colors duration-200 ease-in-out placeholder-boundaryBlack;
+  @apply px-6 py-2 w-9/12 rounded-md border-4 border-boundaryBlack focus:border-gray tracking-widest text-5xl outline-none leading-8 transition-colors duration-200 ease-in-out placeholder-boundaryBlack;
+}
+
+.form-field-error {
+  @apply px-6 py-2 w-9/12 rounded-md border-4 border-red-600 tracking-widest text-5xl outline-none leading-8 placeholder-boundaryBlack;
+}
+
+.form-field-success {
+  @apply px-6 py-2 w-9/12 rounded-md border-4 border-primary tracking-widest text-5xl outline-none leading-8 placeholder-boundaryBlack;
 }
 
 .form-tips {
-  @apply mt-4 text-sm text-gray;
+  @apply mt-2 text-sm text-gray;
 }
 
 .form-error {
-  @apply flex justify-center text-red-500 mt-2;
+  @apply flex h-6 justify-center text-red-500 mb-2;
 }
 
 .form-supplement {
