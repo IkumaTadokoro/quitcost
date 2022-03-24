0cc98e6785621e2f476ad5506b6c926bf64e0bbe
ikuma-t <tadokorodev@gmail.com>
2022-03-24 21:29:04 +0900

    feat(404): 404ページを追加
    Vue Router側で制御している
    参考：[Migrating from Vue 2 \| Vue Router](https://router.vuejs.org/guide/migration/#removed-star-or-catch-all-routes)
    
    Closes: #193

---
+++ app/javascript/src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto pt-24 px-2">
+    <div
+      class="md:max-w-2xl mx-auto p-8 border-4 border-boundaryBlack rounded-xl"
+    >
+      <h1 class="text-xl md:text-3xl text-center mb-4">
+        ご指定のページが見つかりません
+      </h1>
+      <p
+        class="text-lg md:text-2xl text-center mb-6 underline underline-offset-8 decoration-4 decoration-primary"
+      >
+        404: Not Found
+      </p>
+      <p class="text-sm">
+        お探しのページは一時的にアクセスができない状況にあるか、移動もしくは削除された可能性があります。また、URL、ファイル名にタイプミスがないか再度ご確認ください。
+      </p>
+    </div>
+  </div>
+</template>
