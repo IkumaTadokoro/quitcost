b6f03588d22872904de200ed914d9b307011bc59
ikuma-t <tadokorodev@gmail.com>
2022-03-24 20:30:27 +0900

    style(Home): 挿絵を追加
    Figmaで雑に作ったものです

--- app/javascript/src/components/Home.vue
+++ app/javascript/src/components/Home.vue
@@ -39,13 +39,21 @@
             <p class="text-sm md:text-lg text-center mb-2 text-gray">
               住民税決定通知書
             </p>
-            <img src="~dummy.svg" alt="" class="block mx-auto" />
+            <img
+              src="~residence.svg"
+              alt="住民税決定通知書のイラスト"
+              class="block mx-auto shadow-md"
+            />
           </div>
           <div class="w-full md:w-1/2 px-4">
             <p class="text-sm md:text-lg text-center mb-2 text-gray">
-              今年1月から現在までの給与明細
+              今年度の給与明細
             </p>
-            <img src="~dummy.svg" alt="" class="block mx-auto" />
+            <img
+              src="~salary.svg"
+              alt="給与明細のイラスト"
+              class="block mx-auto shadow-md"
+            />
           </div>
         </div>
       </div>
@@ -55,7 +63,8 @@
 
 <script setup>
 import 'logo.svg'
-import 'dummy.svg'
+import 'residence.svg'
+import 'salary.svg'
 import { useRouter } from 'vue-router'
 import { useGlobalStore } from '../store/global'
 
