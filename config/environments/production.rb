9b8d332f0bb9b31028978a2193d185efda2ca1c4
ikuma-t <tadokorodev@gmail.com>
2022-04-29 12:23:22 +0900

    feat: staging環境をホワイトリストに追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.hosts << "quitcost.herokuapp.com"
+  config.hosts << "quitcost-staging.herokuapp.com"
 end
