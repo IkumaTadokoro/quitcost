905a7604f5ea79c8c6ead0ba44e2f3a140eefdea
ikuma-t <tadokorodev@gmail.com>
2022-04-29 11:34:56 +0900

    feat: staging環境用にbasic認証を追加
    Staging環境を作成するには以下の環境変数を指定してください
    - BASIC_AUTH_USER: 任意の値
    - BASIC_AUTH_PASSWORD: 任意の値
    - DB_NAME: quitcost_staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :staging?
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |user, password|
+      user == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
+
+  protected
+
+  def staging?
+    ENV['DB_NAME'] == 'quitcost_staging'
+  end
 end
